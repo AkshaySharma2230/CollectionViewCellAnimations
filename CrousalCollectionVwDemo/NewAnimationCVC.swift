b700ea2cfab3be25af1ce8af1c2188832194c69e
AkshaySharma <akshay.as597@gmail.com>
2025-05-18 22:19:20 +0530

    Adding CollectionView and new controller

---
+++ CrousalCollectionVwDemo/NewAnimationCVC.swift
@@ -0,0 +1,12 @@
+//
+//  NewAnimationCVC.swift
+//  CrousalCollectionVwDemo
+//
+//  Created by Akshay Kumar on 18/05/25.
+//
+
+import UIKit
+
+class NewAnimationCVC: UICollectionViewCell {
+    
+}
---
+++ CrousalCollectionVwDemo/NewAnimationViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NewAnimationViewController.swift
+//  CrousalCollectionVwDemo
+//
+//  Created by Akshay Kumar on 18/05/25.
+//
+
+import UIKit
+
+class NewAnimationViewController: UIViewController {
+
+    @IBOutlet weak var colVw: UICollectionView!
+    
+    //MARK: ViewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colVw.delegate = self
+        colVw.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+}
+
+
+//MARK: UICollectionview Delegate & DataSource Methods
+extension NewAnimationViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewAnimationCVC", for: indexPath) as? NewAnimationCVC {
+            return UICollectionViewCell()
+        }
+        return cell
+    }
+}
